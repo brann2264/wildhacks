1380c128a55be5e1a1cffda25aeb80b3e8c30a81
Kevin Han <kevinhan@dhcp-10-105-37-5.wireless.northwestern.private>
2025-04-05 14:39:41 -0500

    db fixes

--- db.py
+++ db.py
@@ -30,9 +30,14 @@ CREATE TABLE IF NOT EXISTS rankings (
 """
 
 conn = sqlite3.connect("tables.db")
+conn.execute("DROP TABLE IF EXISTS locations")
 conn.execute("CREATE TABLE IF NOT EXISTS locations (id INTEGER PRIMARY KEY, placeid TEXT NOT NULL, name TEXT NOT NULL, lat REAL NOT NULL, lon REAL NOT NULL, address TEXT NOT NULL, sights REAL NOT NULL, nightlife REAL NOT NULL, casual REAL NOT NULL, shopping REAL NOT NULL)")
-conn.execute("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, name TEXT NOT NULL, lat REAL NOT NULL, lon REAL NOT NULL, FOREIGN KEY(rankingid) REFERENCES rankings(userid)))")
-conn.execute("CREATE TABLE IF NOT EXISTS rankings (userid INTEGER PRIMARY KEY, FOREIGN KEY(locationid) REFERENCES locations(id), enjoyment REAL NOT NULL)")
+
+conn.execute("DROP TABLE IF EXISTS users")
+conn.execute("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, name TEXT NOT NULL, lat REAL NOT NULL, lon REAL NOT NULL)")
+
+conn.execute("DROP TABLE IF EXISTS rankings")
+conn.execute("CREATE TABLE IF NOT EXISTS rankings (id INTEGER PRIMARY KEY, userid INTEGER NOT NULL, locationid INTEGER NOT NULL, enjoyment REAL NOT NULL, FOREIGN KEY(userid) REFERENCES users(id), FOREIGN KEY(locationid) REFERENCES locations(id))")
 
 conn.commit()
 conn.close()
